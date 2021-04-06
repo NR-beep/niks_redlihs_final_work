1e719ad4ca6aa01cdc26dd60c5b1bf4c91f5354f
Niks <niks.redlihs@banknote.lv>
2021-04-06 22:11:12 +0300

    copy final work

---
+++ app/src/main/java/finalwork/finalwork/repository/Repository.java
@@ -0,0 +1,14 @@
+package finalwork.finalwork.repository;
+
+import java.util.List;
+
+public interface Repository<T> {
+
+    Long save(T entity);
+
+    List<T> findAll();
+
+    T findById(Long id);
+
+    void delete(Long id);
+}
